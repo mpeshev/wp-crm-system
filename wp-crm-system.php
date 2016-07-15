67b921cf0dc296d8e8449dc3ba001caf3b44192f
scott.deluzio <scott.deluzio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-07-15 22:55:09 +0000

    Commit 1.2.1
    git-svn-id: http://plugins.svn.wordpress.org/wp-crm-system/trunk@1455604 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-crm-system.php
+++ wp-crm-system.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-CRM System
 Plugin URI: https://www.wp-crm.com
 Description: A complete CRM for WordPress
-Version: 1.2.0
+Version: 1.2.1
 Author: Scott DeLuzio
 Author URI: https://www.wp-crm.com
 Text Domain: wp-crm-system
