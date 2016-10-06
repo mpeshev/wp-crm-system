e257431642e368a998b4f464725f1fad7da724f3
scott.deluzio <scott.deluzio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-10-06 17:19:57 +0000

    Updated date settings
    git-svn-id: http://plugins.svn.wordpress.org/wp-crm-system/trunk@1509465 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-crm-system.php
+++ wp-crm-system.php
@@ -247,6 +247,67 @@ function update_wpcrm_contact_names() {
 }
 add_action('wp_ajax_wpcrm_update_contacts', 'update_wpcrm_contact_names');
 function activate_wpcrm_system_settings() {
+	add_option('wpcrm_system_select_user_role', 'manage_options');
+	add_option('wpcrm_system_default_currency', 'USD');
+	add_option('wpcrm_system_report_currency_decimals', 0);
+	add_option('wpcrm_system_report_currency_decimal_point', '.');
+	add_option('wpcrm_system_report_currency_thousand_separator', ',');
+	add_option('wpcrm_system_searchable_dropdown', 'off');
+	add_option('wpcrm_hide_others_posts','no');
+	add_option('wpcrm_system_settings_initial','');
+	add_option('wpcrm_system_email_organization_filter', '');
+
+	$terms = get_terms('contact-type');
+	if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+		foreach ( $terms as $term ) {
+			add_option($term->slug.'-email-filter','');
+		}
+	}
+}
+function deactivate_wpcrm_system_settings() {
+	delete_option('wpcrm_system_select_user_role');
+	delete_option('wpcrm_system_default_currency');
+	delete_option('wpcrm_system_report_currency_decimals');
+	delete_option('wpcrm_system_report_currency_decimal_point');
+	delete_option('wpcrm_system_report_currency_thousand_separator');
+	delete_option('wpcrm_system_searchable_dropdown');
+	delete_option('wpcrm_hide_others_posts');
+	delete_option('wpcrm_system_settings_initial');
+	delete_option('wpcrm_system_date_format');
+	delete_option('wpcrm_system_php_date_format');
+	delete_option('wpcrm_system_email_organization_filter');
+
+	$terms = get_terms('contact-type');
+	if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+		foreach ( $terms as $term ) {
+			delete_option($term->slug.'-email-filter');
+		}
+	}
+}
+function register_wpcrm_system_settings() {
+	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_select_user_role');
+	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_default_currency');
+	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_report_currency_decimals');
+	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_report_currency_decimal_point');
+	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_report_currency_thousand_separator');
+	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_searchable_dropdown');
+	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_hide_others_posts');
+	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_settings_initial');
+	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_date_format');
+	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_php_date_format');
+	register_setting( 'wpcrm_system_email_group','wpcrm_system_email_organization_filter' );
+
+	$terms = get_terms('contact-type');
+	if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+		foreach ( $terms as $term ) {
+			register_setting('wpcrm_system_email_group',$term->slug.'-email-filter');
+		}
+	}
+	add_option( "wpcrm_system_version", "1.2" );
+}
+/* Correct the date format if correct format is not being used */
+add_action('admin_init', 'update_date_formats');
+function update_date_formats() {
 	// Use standard WordPress date format set in Settings > General. Convert PHP date format to jQuery format for date pickers.
 	$php_format = get_option( 'date_format' );
 	$SYMBOLS_MATCHING = array(
@@ -305,81 +366,14 @@ function activate_wpcrm_system_settings() {
 		}
 	}
 
-	add_option('wpcrm_system_select_user_role', 'manage_options');
-	add_option('wpcrm_system_default_currency', 'USD');
-	add_option('wpcrm_system_report_currency_decimals', 0);
-	add_option('wpcrm_system_report_currency_decimal_point', '.');
-	add_option('wpcrm_system_report_currency_thousand_separator', ',');
-	add_option('wpcrm_system_searchable_dropdown', 'off');
-	add_option('wpcrm_hide_others_posts','no');
-	add_option('wpcrm_system_settings_initial','');
-	add_option('wpcrm_system_date_format', $jqueryui_format);
-	add_option('wpcrm_system_php_date_format', $php_format);
-	add_option('wpcrm_system_email_organization_filter', '');
-
-	$terms = get_terms('contact-type');
-	if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-		foreach ( $terms as $term ) {
-			add_option($term->slug.'-email-filter','');
-		}
-	}
-}
-function deactivate_wpcrm_system_settings() {
-	delete_option('wpcrm_system_select_user_role');
-	delete_option('wpcrm_system_default_currency');
-	delete_option('wpcrm_system_report_currency_decimals');
-	delete_option('wpcrm_system_report_currency_decimal_point');
-	delete_option('wpcrm_system_report_currency_thousand_separator');
-	delete_option('wpcrm_system_searchable_dropdown');
-	delete_option('wpcrm_hide_others_posts');
-	delete_option('wpcrm_system_settings_initial');
-	delete_option('wpcrm_system_date_format');
-	delete_option('wpcrm_system_php_date_format');
-	delete_option('wpcrm_system_email_organization_filter');
-
-	$terms = get_terms('contact-type');
-	if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-		foreach ( $terms as $term ) {
-			delete_option($term->slug.'-email-filter');
-		}
-	}
-}
-function register_wpcrm_system_settings() {
-	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_select_user_role');
-	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_default_currency');
-	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_report_currency_decimals');
-	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_report_currency_decimal_point');
-	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_report_currency_thousand_separator');
-	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_searchable_dropdown');
-	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_hide_others_posts');
-	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_settings_initial');
-	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_date_format');
-	register_setting( 'wpcrm_system_settings_main_group', 'wpcrm_system_php_date_format');
-	register_setting( 'wpcrm_system_email_group','wpcrm_system_email_organization_filter' );
-
-	$terms = get_terms('contact-type');
-	if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-		foreach ( $terms as $term ) {
-			register_setting('wpcrm_system_email_group',$term->slug.'-email-filter');
-		}
-	}
-	add_option( "wpcrm_system_version", "1.2" );
-}
-/* Correct the date format if correct format is not being used */
-add_action('admin_init', 'update_date_formats');
-function update_date_formats() {
-	$js_date_format = get_option('wpcrm_system_date_format');
-	$php_date_format = get_option('wpcrm_system_php_date_format');
-	if (trim($js_date_format) == '' || trim($php_date_format) == '') {
-		$js_acceptable = array('yy-M-d','M d, y','MM dd, yy','dd.mm.y','dd/mm/y','d MM yy','D d MM yy','DD, MM d, yy');
-		$php_acceptable = array('Y-M-j','M j, y','F d, Y','d.m.y','d/m/y','j F Y','D j F Y','l, F j, Y');
-		if(!in_array($js_date_format,$js_acceptable) || !in_array($php_date_format,$php_acceptable)) {
-			update_option('wpcrm_system_date_format','MM dd, yy');
-			update_option('wpcrm_system_php_date_format','F d, Y');
-		} else {
-			update_option('wpcrm_system_date_format',$js_date_format);
-			update_option('wpcrm_system_php_date_format',$php_date_format);
-		}
+	$js_acceptable = array('yy-M-d','M d, y','MM dd, yy','dd.mm.y','dd/mm/y','d MM yy','D d MM yy','DD, MM d, yy','MM d, yy','yy-mm-dd','dd/mm/yy','mm/dd/yy');
+	$php_acceptable = array('Y-M-j','M j, y','F d, Y','d.m.y','d/m/y','j F Y','D j F Y','l, F j, Y','F j, Y','Y-m-d','d/m/Y','m/d/Y');
+	if(!in_array($jqueryui_format,$js_acceptable) || !in_array($php_format,$php_acceptable)) {
+		update_option('wpcrm_system_date_format','MM d, yy');
+		update_option('wpcrm_system_php_date_format','F j, Y');
+	} else {
+		update_option('wpcrm_system_date_format',$jqueryui_format);
+		update_option('wpcrm_system_php_date_format',$php_format);
 	}
 }
 /**
